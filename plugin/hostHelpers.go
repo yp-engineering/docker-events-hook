9a316129223763b608d2df5cf26ae036b23e5904
Adam Avilla <aavilla@yp.com>
2015-12-11 07:41:36 -0800

    Adding plugin package to help go pie plugins.

---
+++ plugin/hostHelpers.go
@@ -0,0 +1,26 @@
+package plugin
+
+import (
+	"errors"
+	"net"
+)
+
+func LocalIPAddress() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		return "", err
+	}
+
+	for _, address := range addrs {
+
+		// check the address type and if it is not a loopback, display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return string(ipnet.IP), nil
+			}
+
+		}
+	}
+	return "", errors.New("Can't obtain local IP address.")
+}
---
+++ plugin/inspectHelpers.go
@@ -0,0 +1,51 @@
+package plugin
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/fsouza/go-dockerclient"
+)
+
+func RunningPort(info *docker.Container) (string, error) {
+	var err error
+	switch info.HostConfig.NetworkMode {
+	// `-p` and `-P`
+	case "default":
+		port, err := mappedTCPPort(info)
+		if err == nil {
+			return port, nil
+		}
+	// `--net host` obviously
+	case "host":
+		port, err := exposedTCPPort(info)
+		if err == nil {
+			return port, nil
+		}
+	}
+	return "", err
+}
+
+// Returns the mapped tcp port from docker inspect. See runningPort to see why.
+func mappedTCPPort(info *docker.Container) (string, error) {
+	for key, val := range info.NetworkSettings.Ports {
+		protocol := key.Proto()
+		match, _ := regexp.MatchString("tcp", strings.ToLower(protocol))
+		if match && len(val) > 0 {
+			return val[0].HostPort, nil
+		}
+	}
+	return "", errors.New("no mapped port found.")
+}
+
+// Returns the exposed tcp port from docker inspect. See runningPort to see why.
+func exposedTCPPort(info *docker.Container) (string, error) {
+	for key := range info.Config.ExposedPorts {
+		protocol := key.Proto()
+		match, _ := regexp.MatchString("tcp", strings.ToLower(protocol))
+		if match {
+			return key.Port(), nil
+		}
+	}
+	return "", errors.New("no exposed port found.")
+}
